2aad7bb786584c5a1ab7f77141a65fa9f277f848
U109RXG <U109RXG@mycwt.com>
2026-06-11 16:40:16 +0800

    fix server

--- src/server.js
+++ src/server.js
@@ -18,8 +18,7 @@ app.use('/movies', movieRoutes);
 app.use('/auth', authRoutes);
 app.use('/watchlist', watchlistRoutes);
 
-const PORT = 5001;
-const server = app.listen(PORT, () => {
+const server = app.listen(process.env.PORT || 5001, "0.0.0.0", () => {
   console.log(`server running on port ${PORT}`);
 });
 
